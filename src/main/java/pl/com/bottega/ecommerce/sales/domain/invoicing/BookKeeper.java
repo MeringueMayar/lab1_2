a323d1287c9b162d0451c9040c6a5bb36cd72ca7
Mayar Karkout <204641@edu.p.lodz.pl>
2018-09-07 18:05:40 +0200

    Implemented Factory pattern in Bookmaker class to replace Invoice generation

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -16,14 +16,14 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
-import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import static pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory.createInvoice;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
     public Invoice issuance(ClientData client, List<RequestItem> items) {
-        Invoice invoice = new Invoice(Id.generate(), client);
-
+        Invoice invoice = createInvoice(client);
+        
         for (RequestItem item : items) {
             Money net = item.getTotalCost();
             BigDecimal ratio = null;
